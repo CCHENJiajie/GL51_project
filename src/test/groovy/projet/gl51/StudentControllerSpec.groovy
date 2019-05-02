ec9bfea942d0b014bbd406ff6580d1d0b2497e15
CCHENJiajie <34274642+CCHENJiajie@users.noreply.github.com>
2019-05-02 23:37:00 +0200

    Update StudentControllerSpec.groovy

--- src/test/groovy/projet/gl51/StudentControllerSpec.groovy
+++ src/test/groovy/projet/gl51/StudentControllerSpec.groovy
@@ -3,10 +3,9 @@ package project.gl51
 
 import io.micronaut.context.ApplicationContext
 import io.micronaut.core.type.Argument
-import io.micronaut.runtime.server.EmbeddedServer
-import io.micronaut.http.client.RxHttpClient
-import io.micronaut.http.HttpResponse
 import io.micronaut.http.HttpStatus
+import io.micronaut.http.client.RxHttpClient
+import io.micronaut.runtime.server.EmbeddedServer
 import spock.lang.AutoCleanup
 import spock.lang.Shared
 import spock.lang.Specification
@@ -23,8 +22,7 @@ class StudentControllerSpec extends Specification {
 
         expect:
         response.status == HttpStatus.OK
-        response.body()[0].firstName == 'Sébastien'
-        response.body()[0].lastName == 'Gadot'
-
+        response.body()[0].firstName == 'huang'
+        response.body()[0].lastName == 'sicong'
     }
 }

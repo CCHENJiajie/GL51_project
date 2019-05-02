489c6ca14356b5a270dc4ff741cfee28e2c6eba7
CCHENJiajie <34274642+CCHENJiajie@users.noreply.github.com>
2019-05-02 23:21:17 +0200

    Update StudentController.groovy

--- src/main/groovy/projet/gl51/StudentController.groovy
+++ src/main/groovy/projet/gl51/StudentController.groovy
@@ -2,14 +2,14 @@ package projet.gl51
 
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.HttpStatus
 
 
 @Controller("/student")
-
 class StudentController {
 
     @Get("/")
     List<Student> index() {
-        [new Student (firstname: "HUANG", lastname:"SICONG"), new Student (firstname: "CHEN", lastname:"JIAJIE")]
+        [new Student(firstName: "Sébastien", lastName: "Gadot")]
     }
 }

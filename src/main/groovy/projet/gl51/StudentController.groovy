6ed4b08f30acc0804a9ee59d200ad9f71d761185
CCHENJiajie <chenjiajie_1996@163.com>
2019-05-02 18:59:08 +0200

    test7

--- src/main/groovy/projet/gl51/StudentController.groovy
+++ src/main/groovy/projet/gl51/StudentController.groovy
@@ -1,17 +1,15 @@
 package projet.gl51
 
-import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 
 
 @Controller("/student")
+
 class StudentController {
 
     @Get("/")
     List<Student> index() {
-        [new Student(firstName: "Jiajie1", lastName: "Sicong1")]
-		[new Student(firstName: "Jiajie2", lastName: "Sicong2")]
-		[new Student(firstName: "Jiajie3", lastName: "Sicong3")]
+        [new Student (firstname: "huang", lastname:"Sicong"), new Student (firstname: "Chen", lastname:"Jiajie")]
     }
 }
--- src/test/groovy/projet/gl51/StudentControllerSpec.groovy
+++ src/test/groovy/projet/gl51/StudentControllerSpec.groovy
@@ -1,4 +1,4 @@
-package project.gl51
+package projet.gl51
 
 import io.micronaut.context.ApplicationContext
 import io.micronaut.core.type.Argument
@@ -16,20 +16,15 @@ class StudentControllerSpec extends Specification {
     @Shared @AutoCleanup EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer)
     @Shared @AutoCleanup RxHttpClient client = embeddedServer.applicationContext.createBean(RxHttpClient, embeddedServer.getURL())
 
+
     void "test index"() {
-
         given:
-
         def response = client.toBlocking().exchange("/student", Argument.listOf(Student).type)
-
         expect:
-
         response.status == HttpStatus.OK
-
-        response.body()[1].firstName == "jiajie"
-        response.body()[1].lastName == "sicong"
+        response.body()[1].firstname == "Chen"
+        response.body()[1].lastname == "Jiajie"
         println(response.body)
 
     }
-
 }

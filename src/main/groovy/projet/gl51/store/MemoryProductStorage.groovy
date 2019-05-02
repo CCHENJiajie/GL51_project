2bb1159cfdc8c399f013be0e02ccc71a37a6522c
CCHENJiajie <chenjiajie_1996@163.com>
2019-05-02 18:10:55 +0200

    test1

--- src/main/groovy/projet/gl51/store/MemoryProductStorage.groovy
+++ src/main/groovy/projet/gl51/store/MemoryProductStorage.groovy
@@ -1,28 +1,46 @@
 package gl51.project.store
 
 class MemoryProductStorage implements  ProductStorage {
+
+	List<Product> productList = []
+	Integer id = 1
+	
     @Override
     void save(Product p) {
-
+		p.id = id
+        productList.add(p)
+		id += 1
     }
 
     @Override
     void update(String id, Product p) {
+		def product = getByID(id)
+        def productIndex = productList.indexOf(product)
 
+        p.id = id
+        productList.add(productIndex, p)
+        productList.remove(product)
+        id += 1
     }
 
     @Override
     Product getByID(String id) {
-        return null
+        def product = productList.find { it.id == id }
+        if(product == null)
+        {
+          throw new NotExistingProductException("The wanted product is not exist!")
+        }
+        return product
     }
 
     @Override
     void delete(String id) {
-
+        def product = getByID(id)
+        productList.remove(product)
     }
 
     @Override
     List<Product> all() {
-        []
+        return productList
     }
 }
--- src/test/groovy/projet/gl51/store/MemoryProductStorageTest.groovy
+++ src/test/groovy/projet/gl51/store/MemoryProductStorageTest.groovy
@@ -6,20 +6,80 @@ class MemoryProductStorageTest extends Specification {
 
     ProductStorage store = new MemoryProductStorage()
 
-    def "empty storage returns empty list"() {
+ 	    def "empty storage returns empty list"() {
         expect:
-        store.all() ==  []
+        store.all() == []
     }
 
-    def "adding a product returns the product in the list"() {
+     def "check if adding takes effect"() {
         setup:
-        store.save(new Product(name: "myproduct"))
+        store.save(new Product(name: "myProduct"))
 
-        when:
+         when:
         def all = store.all()
 
-        then:
+         then:
         all.size() == 1
-        all.first().name == 'myproduct'
+        all.first().name == "myProduct"
     }
+
+     def "check if saving takes effect"() {
+        setup:
+        Product myProduct = new Product(name: "myProduct")
+        def id = store.save(myProduct)
+
+         expect:
+        myProduct.id != null
+        myProduct.id == id
+    }
+
+     def "check if deleting takes effect"() {
+        setup:
+        Product myProduct = new Product(name: "myProduct")
+        def id = store.save(myProduct)
+
+         when:
+        store.delete(id)
+
+         then:
+        !store.all().contains(myProduct)
+    }
+
+     def "check if updating takes effect"() {
+        setup:
+        Product myProduct = new Product(name: "myProduct")
+        def id = store.save(myProduct)
+
+         when:
+        Product myUpdatedProduct = new Product(name: "myUpdatedProduct")
+        store.update(id, myUpdatedProduct)
+
+         then:
+        myProduct != myUpdatedProduct
+    }
+
+     def "Throw a NotExistingProductException if we get the id which it doesn't exist"() {
+        setup:
+        def id = -1
+
+         when:
+        store.getByID(id)
+
+         then:
+        thrown NotExistingProductException
+    }
+
+     def "get ID if it exist"() {
+        setup:
+        Product myProduct = new Product(name: "myProduct")
+        def id = store.save(myProduct)
+
+         when:
+        def gettedProduct = store.getByID(id)
+
+         then:
+        myProduct == gettedProduct
+    }
+
+   
 }
